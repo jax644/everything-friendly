125e296734d8a96336ed9bab04a17eabfa720709
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2025-01-17 10:22:39 -0800

    Create dropdown menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,31 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import logo from '../assets/EF-leaf.png';
+import UserMenu from './UserMenu/UserMenu';
 
 
 function Header() {
 
-    const { isAuthenticated, user } = useContext(AuthContext);
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <header>
           {isAuthenticated 
           ? 
-          <p>Logged in as: {user.name}</p> 
+          <UserMenu />
           : 
           <a href="/login"><span>Sign in</span></a>}
-          <a href="/">
-            <h1>Everything-Friendly</h1>
-          </a>
-          <img className="logo" src={logo} alt="Everything-Friendly logo (two leaves resembling the vegetarian symbol on restaurant menus)"/>
+          
+            <div>
+              <a href="/">
+                <h1>Everything-Friendly</h1>
+              </a>
+              <img 
+                className="logo" 
+                src={logo} 
+                alt="Everything-Friendly logo (two leaves resembling the vegetarian symbol on restaurant menus)"
+              />
+          </div>
           <h2>Any recipe, your way.</h2>
         </header>
     );
---
+++ client/src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,41 @@
+import './UserMenu.css';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+function UserMenu() {
+  
+    const navigate = useNavigate();
+    const { user, logout } = useContext(AuthContext);
+
+    console.log(user)
+
+    const handleLogout = () => {
+        console.log('Logging out...')
+        logout();
+        console.log('Redirecting to login page...')
+        navigate('/login');
+    }
+
+    return (
+
+      <div className="dropdown">
+      
+        <div className="dropdown-trigger">
+            <img 
+            src={user.avatar}
+            alt={user.name} 
+            className="avatar"
+            />
+        </div>
+
+        <div className="dropdown-content">
+          <a href="/dashboard">Dashboard</a>
+          <a href="#" onClick={handleLogout}>Sign Out</a>
+        </div>
+
+      </div>
+    );
+  }
+  
+  export default UserMenu;
