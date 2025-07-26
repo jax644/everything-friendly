ca0ee02f247761636b4ab11a61e3745d54c97172
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2025-07-25 19:16:02 -0700

    feat: Simplify recipe component, wrap in Recipe page to get recipe id when needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import GenerateRecipe from "./pages/GenerateRecipe.jsx";
 import Login from "./pages/Login.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import Recipe from "./components/Recipe/Recipe.tsx";
+import RecipePage from "./pages/RecipePage.jsx";
 import "./App.css";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" element={<GenerateRecipe />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/recipe/:id" element={<Recipe />} />
+            <Route path="/recipe/:id" element={<RecipePage />} />
 
             {/* Protected routes */}
             <Route path="/dashboard" element={<Dashboard />} />
